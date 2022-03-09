26fd331189f15dd1a7f5401f2965e58e49c5121e
Kitorosano <rosanoesteban2@gmail.com>
2022-03-09 11:18:39 -0300

    first commit

---
+++ src/test/java/uy/com/sofka/retoddd/RetodddApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.sofka.retoddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetodddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
